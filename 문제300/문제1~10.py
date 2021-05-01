fcd5ccc6a45244e33796e4ced74e61904a820d83
dkswnstn <dkswnstn@gmail.com>
2021-05-01 17:34:22 +0900

    2021-05-01

---
+++ 문제300/문제1~10.py
@@ -0,0 +1,20 @@
+#-001
+print("hello word")
+#002
+print("Mary's cosmetics")
+#003
+print('신씨가 소리를 질렀다"도둑이야"')
+#004
+print("C:/windows")
+#005
+print("안녕하세요","n만나서","반갑습니다")
+#006
+print("오늘은","일요일")
+#007
+print("naver;kakao;samsung",sep=";")
+#008
+print("never/","kakao/","samsung")
+#009
+print(end="first");print("second")
+#010
+print("5/3")
---
+++ 문제300/문제21~30.py
@@ -0,0 +1,21 @@
+#21
+Letters='python'
+print(Letters[0],Letters[2])
+
+#22
+License_plate="24가 2210"
+print(License_plate[-4:])
+print(License_plate[4:8])
+
+#23
+string="홀짝홀짝홀짝"
+print(string[0:6:2])
+
+#24
+string="PYTHON"
+print(string[::-1])
+
+#25
+phone_number="010-1111-2222"
+phone_number1=phone_number.replace("-"," ")
+print(phone_number1)
