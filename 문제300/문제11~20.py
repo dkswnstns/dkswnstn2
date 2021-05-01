2ae4b52f6935c07ad73af852b02089de19b8d910
dkswnstn <dkswnstn@gmail.com>
2021-05-01 17:36:23 +0900

    2021-05-01

--- 문제300/문제11~20.py
+++ 문제300/문제11~20.py
@@ -11,21 +11,37 @@ print("시가총액")
 s="hello"
 t="python"
 print(s,t)
+
 #014
 print(2+2*3)
+
 #015
 a = 128
 print(type(a))
 
 #016
+nums_str="720"
+nums_int=int(nums_str)
+print(nums_int)
 
 #017
 num=100
-print("num")
+nums_str=str(num)
+print(nums_str)
+
 #018
 문자열="15.79"
-print("문자열")
+실수형=float(문자열)
+print(실수형)
 
 #019
+year="2020"
+print(int("2020"))
+print(int("2020")+1)
+print(int("2020")+2)
 
 #020
+에어컨=48,584
+무의자=36
+총금액=에어컨*무의자
+print(총금액)
